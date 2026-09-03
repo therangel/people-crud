f325a92f48c1a4171d7059d24359b052d1001e7b
therangel <229519868+therangel@users.noreply.github.com>
2026-09-03 17:47:14 -0300

    update: Menu button bugs fixed.

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,34 +2,32 @@ const menuButton = document.getElementById("burger-btn")
 const sidebar = document.querySelector(".sidebar")
 const overlay = document.querySelector(".overlay")
 const collapseSidebarButton = document.querySelector(".collapse-sidebar")
-// const openSidebarButton = document.querySelector(".open-sidebar")
+const closeSidebarButton = document.querySelector(".close-sidebar")
 const homePageLink = document.querySelector(".home")
 const clientPageLink = document.querySelector(".client")
 
 
-function toggleMenu() {
-    sidebar.classList.toggle("active")
-    menuButton.classList.toggle("active")
-    overlay.classList.toggle("active")
+function openSidebar() {
+    sidebar.classList.add("active")
+    overlay.classList.add("active")
 }
 
 function closeMenu() {
     sidebar.classList.remove("active")
-    menuButton.classList.remove("active")
     overlay.classList.remove("active")
 }
 
-function toggleSidebar() {
-
+function collapseSidebar() {
     sidebar.classList.toggle("collapsed")
     collapseSidebarButton.classList.toggle("collapsed")
 }
 
 
-menuButton.addEventListener("click", toggleMenu)
+menuButton.addEventListener("click", openSidebar)
 overlay.addEventListener("click", closeMenu)
+closeSidebarButton.addEventListener("click", closeMenu)
 
-collapseSidebarButton.addEventListener("click", toggleSidebar)
+collapseSidebarButton.addEventListener("click", collapseSidebar)
 
 
 
